2d4a25a619e4b5d7a1b6d0a1678d30d4076885f9
Melina <melinamone@gmail.com>
2026-05-01 17:50:36 -0300

    ...

--- SistemaDistribuidora/src/main/java/com/distribuidora/service/DistribuidoraService.java
+++ SistemaDistribuidora/src/main/java/com/distribuidora/service/DistribuidoraService.java
@@ -1,4 +1,95 @@
 package com.distribuidora.service;
 
+import com.distribuidora.data.DistribuidoraRepository;
+import com.distribuidora.model.Producto;
+import com.distribuidora.model.Proveedor;
+
+import java.util.List;
+
 public class DistribuidoraService {
+
+    private static DistribuidoraService instancia;
+    private DistribuidoraRepository repository;
+
+    private DistribuidoraService() {
+        this.repository = DistribuidoraRepository.getInstancia();
+    }
+
+    public static DistribuidoraService getInstancia() {
+        if (instancia == null) {
+            instancia = new DistribuidoraService();
+        }
+        return instancia;
+    }
+
+    public void registrarProveedor(String razonSocial, String cuit, String telefono, String email) {
+        Proveedor proveedor = new Proveedor();
+        proveedor.setRazonSocial(razonSocial);
+        proveedor.setCuit(cuit);
+        proveedor.setTelefono(telefono);
+        proveedor.setEmail(email);
+
+        repository.guardarProveedor(proveedor);
+        System.out.println("Proveedor registrado correctamente");
+    }
+
+    public void registrarProducto(String nombre, String categoria, Double precioUnitario, int stock, Long idProveedor) {
+        Proveedor proveedor = repository.buscarProveedorPorId(idProveedor);
+        if (proveedor == null) {
+            System.out.println("Error: no existe un proveedor con Id" + idProveedor);
+            return;
+        }
+        Producto producto = new Producto();
+        producto.setNombre(nombre);
+        producto.setCategoria(categoria);
+        producto.setPrecioUnitario(precioUnitario);
+        producto.setStock(stock);
+        producto.setProveedor(proveedor);
+
+        repository.guardarProducto(producto);
+        System.out.println("Producto registrado correctamente");
+    }
+
+    public void buscarProductos(String categoria, Double precioMax, Integer stockMin){
+        List<Producto> productos= repository.buscarProductos(categoria,precioMax,stockMin);
+
+        if(productos.isEmpty()){
+            System.out.println("No se enontraron productos con esos filtros");
+            return;
+        }
+
+        System.out.println("==PRODUCTOS ENCONTRADOS==");
+        for(Producto producto:productos){
+            System.out.println("ID: " + producto.getId());
+            System.out.println("Nombre: " + producto.getNombre());
+            System.out.println("Categoría: " + producto.getCategoria());
+            System.out.println("Precio unitario: $" + producto.getPrecioUnitario());
+            System.out.println("Stock: " + producto.getStock());
+            System.out.println("Proveedor: " + producto.getProveedor().getRazonSocial());
+            System.out.println("-----------------------------");
+        }
+    }
+
+    public void mostrarResumenPorProveedor(){
+        List<Object[]> resumen= repository.resumenPorProveedor();
+
+        if(resumen.isEmpty()){
+            System.out.println("No hay productos cargados para mostrar resumen.");
+            return;
+        }
+
+        System.out.println("==RESUMEN POR PROVEEDOR==");
+        for(Object[] fila: resumen){
+            Proveedor proveedor=(Proveedor) fila[0];
+            Long cantidadProductos = (Long) fila[1];
+            Long stockTotal = (Long) fila[2];
+            Double valorTotal = (Double) fila[3];
+
+            System.out.println("Proveedor: " + proveedor.getRazonSocial());
+            System.out.println("Cantidad de productos: " + cantidadProductos);
+            System.out.println("Stock total: " + stockTotal);
+            System.out.println("Valor total inventario: $" + valorTotal);
+            System.out.println("-----------------------------");
+        }
+    }
 }
--- SistemaDistribuidora/src/main/java/org/example/Main.java
+++ SistemaDistribuidora/src/main/java/org/example/Main.java
@@ -1,17 +1,97 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.distribuidora.data.HibernateUtil;
+import com.distribuidora.service.DistribuidoraService;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Crea un objeto para leer lo que escribe el usuario
+        Scanner scanner = new Scanner(System.in);
+        String nombre =scanner.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        DistribuidoraService service= DistribuidoraService.getInstancia();
+
+        int opcion;
+        do {
+            System.out.println("=== Distribuidora de Alimentos ===");
+            System.out.println("1. Registrar proveedor");
+            System.out.println("2. Registrar producto");
+            System.out.println("3. Buscar productos");
+            System.out.println("4. Ver resumen por proveedor");
+            System.out.println("0. Salir");
+
+            System.out.println("Seleccione una opcion:");
+            //lee lo que el usuario escribe y convierte el texto a número
+            opcion=Integer.parseInt(scanner.nextLine());
+
+            //según lo que eligio:
+            switch (opcion){
+                case 1:
+                    System.out.println("Razon social: ");
+                    String razonSocial = scanner.nextLine();
+
+                    System.out.print("CUIT: ");
+                    String cuit = scanner.nextLine();
+
+                    System.out.print("Teléfono: ");
+                    String telefono = scanner.nextLine();
+
+                    System.out.print("Email: ");
+                    String email = scanner.nextLine();
+
+                    service.registrarProveedor(razonSocial, cuit, telefono, email);
+                    break;
+                case 2:
+                    System.out.print("Nombre del producto: ");
+                    String nombreProd = scanner.nextLine();
+
+                    System.out.print("Categoría: ");
+                    String categoria = scanner.nextLine();
+
+                    System.out.print("Precio unitario: ");
+                    double precioUnitario = Double.parseDouble(scanner.nextLine());
+
+                    System.out.print("Stock: ");
+                    int stock = Integer.parseInt(scanner.nextLine());
+
+                    System.out.print("ID del proveedor: ");
+                    Long idProveedor = Long.parseLong(scanner.nextLine());
+
+                    service.registrarProducto(nombreProd, categoria, precioUnitario, stock, idProveedor);
+                    break;
+                case 3:
+                    System.out.print("Categoría (enter para omitir): ");
+                    String categoriaFiltro = scanner.nextLine();
+                    if (categoriaFiltro.isBlank()) categoriaFiltro = null;
+
+                    System.out.print("Precio máximo (enter para omitir): ");
+                    String precioTexto = scanner.nextLine();
+                    Double precioMax = null;
+                    if (!precioTexto.isBlank()) precioMax = Double.parseDouble(precioTexto);
+
+                    System.out.print("Stock mínimo (enter para omitir): ");
+                    String stockTexto = scanner.nextLine();
+                    Integer stockMin = null;
+                    if (!stockTexto.isBlank()) stockMin = Integer.parseInt(stockTexto);
+
+                    service.buscarProductos(categoriaFiltro, precioMax, stockMin);
+                    break;
+                case 4:
+                    service.mostrarResumenPorProveedor();
+                    break;
+                case 0:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+                    break;
+            }
+            System.out.println();
+        }while (opcion!=0);
+            scanner.close();
+            HibernateUtil.shutdown();
+
     }
 }
