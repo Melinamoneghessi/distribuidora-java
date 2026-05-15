8d843e35f9b83d711e91d82059bd74dcbd425e63
Melina <melinamone@gmail.com>
2026-05-15 13:50:38 -0300

    CI Workflows

---
+++ SistemaDistribuidora/src/test/java/com/distribuidora/data/TestSimple.java
@@ -0,0 +1,13 @@
+package com.distribuidora.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestSimple {
+
+    @Test
+    public void prueba() {
+        assertTrue(true);
+    }
+}
